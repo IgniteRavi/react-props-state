d7a6b5efa7b08be847ee5378c12080c7a4298a87
Ravi Gupta <ravi123.niec@gmail.com>
2019-02-05 23:09:28 +0400

    Update index.js
    componentdidmount & componentdidupdate method usage

--- src/index.js
+++ src/index.js
@@ -34,6 +34,15 @@ class Library extends Component {
         freeBookMark: true,
         hiring: false
     }
+
+    componentDidMount() {
+        console.log("The component is mounted.")
+    }
+
+    componentDidUpdate() {
+        console.log("The component just updated.")
+    }
+
     toggleOpenClosed = () => {
         this.setState(prevState => ({
             open: !prevState.open
