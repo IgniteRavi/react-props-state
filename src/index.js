a6688ba83cd4ac61582b28cec951c9a6f0de0676
Ravi Gupta <ravi123.niec@gmail.com>
2019-02-05 22:46:01 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import {render} from 'react-dom'
+
+let bookList = [
+    {"title":"A Little Life", "author":"Hanya Yanagihara", "pages":505},
+    {"title":"Two From the Heart", "author":"Hanya Yanagihara", "pages":505},
+    {"title":"Five Point Someone", "author":"Hanya Yanagihara", "pages":505}
+]
+
+const Book = ({title, author, pages, freeBookMark}) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <p>by: {author}</p>
+            <p>Pages: {pages} pages</p>
+            <p>Free Bookmark Today: {freeBookMark ? 'Yes!' : 'No!'}</p>
+        </section>
+    )
+}
+
+const Hiring = () =>
+    <div>
+        <p>The library is hiring. Go to www.library.com for more details.</p>
+    </div>
+
+const NotHiring = () =>
+    <div>
+        <p>The library is not hiring. Check back later for more info.</p>
+    </div>
+
+class Library extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            open: true,
+            freeBookMark: true,
+            hiring: false
+        }
+        this.toggleOpenClosed = this.toggleOpenClosed.bind(this)
+    }
+    toggleOpenClosed() {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
+    }
+
+    render(){
+        const {books} = this.props
+        return (
+            <div>
+                {this.state.hiring ? <Hiring/> : <NotHiring/>}
+                <h1>The library is {this.state.open ? 'open':'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                {books.map(
+                    (book, i) => <Book 
+                        key={i} 
+                        title={book.title} 
+                        author={book.author} 
+                        pages={book.pages} 
+                        freeBookMark={this.state.freeBookMark}/>
+                )}  
+            </div>
+        )
+    }
+}
+
+render(
+    <Library books={bookList}/>,
+    document.getElementById('root')
+)
